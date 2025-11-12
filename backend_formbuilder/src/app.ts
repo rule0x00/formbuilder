9a79f7449395e6400d98b36638c306b7820a84cb
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-11-12 13:52:18 +0530

    app

--- backend_formbuilder/src/app.ts
+++ backend_formbuilder/src/app.ts
@@ -1,10 +1,17 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 import connect_to_db from "./utils/db_connection"
 import userRouter from "./routes/user_router"
 import formRouter from "./routes/form_router"
+import adminFormrouter from "./routes/admin_form_router"
 import {authenticate_user} from "./middleware/auth"
+import expressLayouts from "express-ejs-layouts";
+import cookieParser from "cookie-parser";
+import methodOverride from "method-override";
+
+
 
 dotenv.config()
 
@@ -14,18 +21,37 @@ const app = express()
 //cors middleware
 app.use(cors())
 
-//json body parser
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.use(cookieParser());
 
 connect_to_db()
+
+app.use((req, res, next) => {
+    res.locals.user = (req as any).user || null;
+    next();
+});
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../src/views"));
+
+
+app.use(expressLayouts);
+app.set("layout", "layouts/layout"); // default layout file
+
+// Static folder for CSS
+app.use(express.static(path.join(__dirname, "../src/public")));
 
 // Auth middleware
 // app.use(authenticate_user)
 
 app.use("/forms", authenticate_user, formRouter)
+app.use("/admin/forms", authenticate_user, adminFormrouter);
 app.use("/account", userRouter)
-app.use("/", (_req, res) => {
-    res.send("FORMBUILDER")
+app.use("/",authenticate_user, (req: Request, res: Response) => {
+    res.render("index", { title: "Home", user: (req as any).user });
 })
 
 
