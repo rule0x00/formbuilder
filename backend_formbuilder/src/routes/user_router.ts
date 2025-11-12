bf6f94f58d56878d90bd6fd606d0c81bbcc1bacf
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-11-12 13:54:54 +0530

    user router

--- backend_formbuilder/src/routes/user_router.ts
+++ backend_formbuilder/src/routes/user_router.ts
@@ -3,6 +3,15 @@ import { handle_user_login, handle_user_signup } from "../controllers/users"
 
 const router = express.Router()
 
+router.get("/login", (_req, res) => {
+    res.render("account/login", { title: "Login", error: null });
+});
+
+// Render signup page
+router.get("/signup", (_req, res) => {
+    res.render("account/signup", { title: "Signup", error: null });
+});
+
 router.post("/signup", handle_user_signup)
 router.post("/login", handle_user_login)
 
